153146d7d6bdb5d482292fc61556d47f6bac4414
Sebastian L <creepergamer6834@gmail.com>
2026-09-04 15:50:13 -0700

    k

---
+++ Rectangle.java
@@ -0,0 +1,28 @@
+public class Rectangle {
+    private int length;
+    private int width;
+
+    public Rectangle(int length, int width) {
+
+    }
+
+    public Rectangle() {
+
+    }
+
+    public int getLength() {
+
+    }
+
+    public int getWidth() {
+
+    }
+
+    public void setLength(int length) {
+
+    }
+
+    public void setWidth(int width) {
+
+    }
+}
