b777b77b2229190ac772440ee742c60ba37dd716
Sebastian L <creepergamer6834@gmail.com>
2026-09-09 09:10:00 -0700

    a

--- Rectangle.java
+++ Rectangle.java
@@ -3,26 +3,53 @@ public class Rectangle {
     private int width;
 
     public Rectangle(int length, int width) {
-
+        this.length = length;
+        this.width = width;
     }
 
     public Rectangle() {
-
+        length = 5;
+        width = 10;
     }
 
     public int getLength() {
-
+        return length;
     }
 
     public int getWidth() {
-
+        return width;
     }
 
     public void setLength(int length) {
-
+        length = this.length;
     }
 
     public void setWidth(int width) {
+        width = this.width;
+    }
 
+    public int calculateArea() {
+        int area = length * width;
+        return area;
+    }
+
+    public int calculatePerimeter() {
+        int perimeter = length * 2 + width * 2;
+        return perimeter;
+    }
+
+    public double calculateDiagonal() {
+        double diagonal = Math.pow(length, 2) + Math.pow(width, 2);
+        diagonal = Math.pow(diagonal, 0.5);
+        return diagonal;
+    }
+
+    public String toString() {
+        String data = "Width: " + width + "\nLength: " + length;
+        return data;
+    }
+
+    public boolean equals(Rectangle other) {
+        return length == other.length && width == other.width;
     }
 }
--- RectangleTester.java
+++ RectangleTester.java
@@ -1,3 +1,17 @@
 public class RectangleTester {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle(5, 10);
+        System.out.println(rect.toString());
+        System.out.println("Area: " + rect.calculateArea());
+        System.out.println("Perimeter: " + rect.calculatePerimeter());
+        System.out.println("Diagonal: " + rect.calculateDiagonal());
+        System.out.println("Equal: " + rect.equals(rect));
 
+        Rectangle rect2 = new Rectangle(0, 0);
+        System.out.println(rect2.toString());
+        System.out.println("Area: " + rect2.calculateArea());
+        System.out.println("Perimeter: " + rect2.calculatePerimeter());
+        System.out.println("Diagonal: " + rect2.calculateDiagonal());
+        System.out.println("equal: " + rect.equals(rect2));
+    }
 }
